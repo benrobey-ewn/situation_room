86a22be7ec7aeec2aa725d88ff031de1e69b4a9d
Ben Robey <ben.robey@ewn.com.au>
2016-11-14 01:37:31 +1000

    remove stuff

--- staging/ajax/get_forecast_storm.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo $page_data = file_get_contents('http://cloud2.stormcast.com.au/stormcast/map.interface.times.php?format=json');
-?>
--- staging/logout.php
+++
@@ -1,8 +0,0 @@
-<?php include 'includes/config.php';
-if(!empty($_SESSION['user_id'])) {
-   // destroy the session
-    session_destroy(); 
-}
-    header('Location:login.php');
-    die;
-?>
